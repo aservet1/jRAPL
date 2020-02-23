774f6858b22b70ba4196773859c58387677e808d
Alejandro Servetto <aservet1@binghamton.edu>
2020-02-22 21:32:36 -0500

    made it loop and count

--- jRaplSourceCode/DriverAlejandro.java
+++ jRaplSourceCode/DriverAlejandro.java
@@ -4,39 +4,49 @@ import java.util.Arrays;
 
 public class DriverAlejandro
 {
+	public static void repeatGetEnergyStats(int times)
+	{
+		for (int i = 0;; i++)
+		{
+			EnergyCheckUtils.getEnergyStats();
+			try { Thread.sleep(1000L); } catch (Exception e) { System.out.println("yikes"); }
+		}
+	}
+
 	public static void main(String[] args)
 	{
-		long startTime, endTime;
+		repeatGetEnergyStats(10);
+	/*	long startTime, endTime;
 
-			System.out.println("\n--------------\n < Running static block:\n");
-			startTime = System.currentTimeMillis();
-			EnergyCheckUtils e = new EnergyCheckUtils();
-			endTime = System.currentTimeMillis();
-			System.out.println(" < Static block took " + (endTime - startTime) + " ms");
+		System.out.println("\n--------------\n < Running static block:\n");
+		startTime = System.currentTimeMillis();
+		EnergyCheckUtils e = new EnergyCheckUtils();
+		endTime = System.currentTimeMillis();
+		System.out.println(" < Static block took " + (endTime - startTime) + " ms");
 
-			System.out.println("\n\n < Running ProfileInit():\n");
-			startTime = System.currentTimeMillis();
-			EnergyCheckUtils.ProfileInit();
-			endTime = System.currentTimeMillis();
-			System.out.println(" <  Profileinit() took " + (endTime - startTime) + " ms");
+		System.out.println("\n\n < Running ProfileInit():\n");
+		startTime = System.currentTimeMillis();
+		EnergyCheckUtils.ProfileInit();
+		endTime = System.currentTimeMillis();
+		System.out.println(" <  Profileinit() took " + (endTime - startTime) + " ms");
 
-			System.out.println("\n--------------\n < Running GetSocketNum():\n");
-			startTime = System.currentTimeMillis();
-			EnergyCheckUtils.GetSocketNum();
-			endTime = System.currentTimeMillis();
-			System.out.println(" < GetSocketNum() took " + (endTime - startTime) + " ms");
+		System.out.println("\n--------------\n < Running GetSocketNum():\n");
+		startTime = System.currentTimeMillis();
+		EnergyCheckUtils.GetSocketNum();
+		endTime = System.currentTimeMillis();
+		System.out.println(" < GetSocketNum() took " + (endTime - startTime) + " ms");
 
-			System.out.println("\n--------------\n < Running EnergyStatCheck():\n");
-			startTime = System.currentTimeMillis();
-			EnergyCheckUtils.EnergyStatCheck();
-			endTime = System.currentTimeMillis();
-			System.out.println(" < EnergyStatCheck() took " + (endTime - startTime) + " ms");
+		System.out.println("\n--------------\n < Running EnergyStatCheck():\n");
+		startTime = System.currentTimeMillis();
+		EnergyCheckUtils.EnergyStatCheck();
+		endTime = System.currentTimeMillis();
+		System.out.println(" < EnergyStatCheck() took " + (endTime - startTime) + " ms");
 
-			System.out.println("\n--------------\n < Running getEnergyStats():\n");
-			startTime = System.currentTimeMillis();
-			double[] stats = EnergyCheckUtils.getEnergyStats();
-			endTime = System.currentTimeMillis();
-			System.out.println(" < getEnergyStats() took " + (endTime - startTime) + " ms");
-
+		System.out.println("\n--------------\n < Running getEnergyStats():\n");
+		startTime = System.currentTimeMillis();
+		double[] stats = EnergyCheckUtils.getEnergyStats();
+		endTime = System.currentTimeMillis();
+		System.out.println(" < getEnergyStats() took " + (endTime - startTime) + " ms");
+	*/
 	}
 }
