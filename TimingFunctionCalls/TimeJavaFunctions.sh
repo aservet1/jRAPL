1bf262517896a72caf6461237b4e8f15f52f3a4a
aservet1 <54599694+aservet1@users.noreply.github.com>
2020-03-22 23:29:16 -0400

    Update TimeJavaFunctions.sh

--- TimingFunctionCalls/TimeJavaFunctions.sh
+++ TimingFunctionCalls/TimeJavaFunctions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-# assumes the java and c files are properly set up for java side timing
+# assumes the java and c files are properly set up for Java side timing
 sudo java jrapl.DriverAlejandro 1000 |			\
 	grep -E 'Results|Avg|StDev|---' |		\
 	sed 's/.*---/------------------------------/'	\
