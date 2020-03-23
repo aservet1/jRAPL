b970c69946c425a563b9c0a591e180374e1fc2f9
Alejandro Servetto <aservet1@binghamton.edu>
2020-03-23 17:33:39 -0400

    started doign time graphs, realized that java trial num need to be 4000 to match C trial num, pushing so rutvik can run the updated TimeJava Script

--- TimingFunctionCalls/TimeJavaFunctions.sh
+++ TimingFunctionCalls/TimeJavaFunctions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cd ..
+trials=4000
+
+sudo modprobe msr
+cd ../jRaplSourceCode
 # assumes the java and c files are properly set up for Java side timing
-sudo java jrapl.DriverAlejandro 1000 |			\
+sudo java jrapl.DriverAlejandro $trials |			\
 	grep -E 'Results|Avg|StDev|---' |		\
 	sed 's/.*---/------------------------------/'	\
-	> alejandro-test-place/java-AVG_STDEV.data
+	> ../TimingFunctionCalls/java-AVG_STDEV.data
