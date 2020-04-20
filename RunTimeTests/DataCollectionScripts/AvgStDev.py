1eda6b543398c0c9d248f99f5fad329f22608968
Gitea <aservet1@binghamton.edu>
2020-04-19 23:43:34 -0400

    Started Organizing RunTime test folder. Archived Old Data, still need to reconfigure the scripts

---
+++ RunTimeTests/DataCollectionScripts/AvgStDev.py
@@ -0,0 +1,67 @@
+from math import sqrt
+from sys import argv
+from time import sleep
+from random import randint
+'''------------------------------------'''
+
+def remove_non_numeric_characters(s):
+	int_string = ''
+	for c in s:
+		if c.isnumeric():
+			int_string += c
+	return int_string
+
+'''------------------------------------'''
+def avg(l):
+	try:
+		retval = sum(l)/len(l)
+	except:
+		print("Divided by zero probably")
+		return 0
+	return 
+
+def deviations(l):
+	av = avg(l)
+	devs = []
+	for x in l:
+		devs.append(x-av)
+	return devs
+
+def stdev(l):
+	devs = deviations(l)
+	S = 0
+	for d in devs:
+		S += d**2
+	try:
+		S /= (len(l)-1)
+		S = sqrt(S)
+	except:
+		print("Divided by zero probably")
+		return 0
+	return S
+
+'''------------------------------------'''
+
+if (len(argv) < 2):
+	print("usage: python3 " + argv[0] + " inputFile")
+	exit(1)
+
+fileName = argv[1]
+
+dataFile = open(fileName,'r')
+stringData = dataFile.readlines()
+dataFile.close()
+
+intData = []
+for s in stringData:
+	s = s.strip()
+	ints_from_s = remove_non_numeric_characters(s)
+	if (ints_from_s == ''): continue
+	intData.append(int(ints_from_s))
+
+average = avg(intData)
+standard_deviation = stdev(intData)
+
+print('Results from file: \''+fileName+"\'")
+print("Avg:\t",average)
+print("StDev:\t",standard_deviation)
