b8e3eb1c661b4529cf13be38961bae43c435a887
Gitea <aservet1@binghamton.edu>
2020-05-17 15:58:08 -0400

    delay set via cmdline arg

--- src/GeneralTestDriver.java
+++ src/GeneralTestDriver.java
@@ -39,8 +39,11 @@ public class GeneralTestDriver
 
 	public static void main(String[] args)
 	{
+		int d;
+		if (args.length == 0) d = 10;
+		else d = Integer.parseInt(args[0]);
 		EnergyReadingCollector ec = new EnergyReadingCollector();
-		ec.setDelay(10);
+		ec.setDelay(d);
 		int[] arr = randarr(11);
 
 		new Thread(ec).start();
