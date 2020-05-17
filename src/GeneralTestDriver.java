9ebf6c6d6c4072ff52f323c7f5e88288620f45bc
Gitea <aservet1@binghamton.edu>
2020-05-17 15:46:10 -0400

    demonstrating the energy reading collector as it works on a bogoSort

--- src/GeneralTestDriver.java
+++ src/GeneralTestDriver.java
@@ -1,21 +1,53 @@
 package jrapl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GeneralTestDriver
 {
+	private static boolean isSorted(int[] arr)
+	{
+		for (int i = 1; i < arr.length; i++)
+			if (arr[i] < arr[i-1])
+				return false;
+		return true;
+	}
+
+	private static void shuffle(int[] arr)
+	{
+		for (int x = 0; x < arr.length*2; x++) {
+			int i = (int)(Math.random()*arr.length);
+			int j = (int)(Math.random()*arr.length);
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+
+	private static void bogoSort(int[] arr)
+	{
+		while (!isSorted(arr)) shuffle(arr);
+	}
+
+	private static int[] randarr(int length)
+	{
+		int[] arr = new int[length];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = (int)(Math.random()*10)+1;
+		return arr;
+	}
+
 	public static void main(String[] args)
 	{
-		/*ArrayList<Integer> al = new ArrayList<Integer>();
-		for (int x = 0; x < 97; x++) al.add(x);
-		System.out.println(al.size());*/
 		EnergyReadingCollector ec = new EnergyReadingCollector();
 		ec.setDelay(10);
-		
+		int[] arr = randarr(11);
+
 		new Thread(ec).start();
-		try { Thread.sleep(1000); } catch (Exception e) {}
+		bogoSort(arr);
 		ec.end();
 
+		System.out.println(Arrays.toString(arr)+"\n");
 		System.out.println(ec);
 	}
 
