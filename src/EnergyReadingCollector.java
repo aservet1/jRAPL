201570e16b36b5d381d42d07decb375eb25f89d3
Gitea <aservet1@binghamton.edu>
2020-05-29 14:49:41 -0400

    tested, added thread.join() to stopReading(), did javadoccing

--- src/EnergyReadingCollector.java
+++ src/EnergyReadingCollector.java
@@ -5,11 +5,20 @@ import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
 
+
+/**
+*	@author Alejandro Servetto
+*	Object that takes and stores energy readings over a set delay (milliseconds)
+*	Runs as a thread in the background between this.startReading() and this.stopReading()
+*	Every individual energy reading is the energy consumed (joules) over the course of the delay
+*	Energy read from three power domains: DRAM, CPU core, CPU package
+*/
 public class EnergyReadingCollector implements Runnable
 {
 	private ArrayList<double[]> readings; 
 	private int delay; // milliseconds
 	private volatile boolean exit = false;
+	private Thread t = null;
 
 	public EnergyReadingCollector()
 	{
@@ -35,36 +44,64 @@ public class EnergyReadingCollector implements Runnable
 		return reading;
 	}
 
+	private String labelledReading(double[] reading)
+	{
+		return "dram: " + reading[0] + "\tcore: " + reading[1] + "\tpkg: " + reading[2];
+	}
+
 	public void run()
 	{
 		while (!exit)
 		{
 			double[] reading = readOverDelay();
-			if (!exit) { // in case exit happened while readOverDelay()
-				readings.add(reading);
-			}
+			readings.add(reading);
 		}
 	}
 
-	//runs in the background
+	/**
+	*	Starts a thread with this object. Continually takes and stores energy readings
+	*	in the background while main thread runs. Will run until this.stopReading() is called
+	*/
 	public void startReading()
 	{
-		new Thread(this).start();
+		t = new Thread(this);
+		t.start();
 	}
 
+	/**
+	*	Stops the thread this object is running. Sets the 'exit' flag to true,
+	*	which ends the loop in the run() method.
+	*/
 	public void stopReading()
 	{
 		exit = true;
+		try {
+			 t.join();
+		} catch (Exception e) {
+			System.out.println("Exception " + e + " caught.");
+			e.printStackTrace();
+		}
+		t = null;
 	}
 
-	// run this before reusing the same EnergyReadingCollector
-	// make sure to save relevant info from readings because this resets
+	/**
+	*	Resets the object for reuse.
+	*	Call this if you intend to reuse the same object for energy collection after already using it.
+	*	Clears out the current list of readings stored in the object.
+	*/
 	public void reInit()
 	{
 		exit = false;
 		readings.clear();
 	}
 
+	/**
+	*	Returns K most recent readings. Each readings is a double[] of the form
+	*	[dram energy, core energy, package energy]. If K is greater than the amount
+	*	of readings, simply returns all readings
+	*	@param k: Number of most recent readings
+	*	@return An array of the K most recent readings.
+	*/
 	public double[][] getLastKReadings(int k)
 	{
 		int start = readings.size() - k;
@@ -82,22 +119,36 @@ public class EnergyReadingCollector implements Runnable
 		return readings_array;
 	}
 	
+	/**
+	*	@return The current delay (in milliseconds)
+	*/
 	public int getDelay()
 	{
 		return delay;
 	}
 
+	/**
+	*	@param d: Sets the delay for this object (in milliseconds)
+	*/
 	public void setDelay(int d)
 	{
 		delay = d;
 	}
 
-	private String labelledReading(double[] reading)
+	/**
+	*	@return number of readings currently stored in the object
+	*/
+	public int getNumReadings()
 	{
-		return "dram: " + reading[0] + "\tcpu: " + reading[1] + "\tpkg: " + reading[2];
+		return readings.size();
 	}
 
-	//dump to file
+	/**
+	*	Dumps all readings to file, along with the delay between readings. Format:
+	*	Same format as toString(). @see toString()
+	*	
+	*	@param fileName: name of file to write to
+	*/
 	public void writeReadingsToFile(String fileName)
 	{
 		FileWriter writer = null;
@@ -111,10 +162,21 @@ public class EnergyReadingCollector implements Runnable
 		}
 	}
 
+	/**
+	*	Returns a string representation of the object
+	*	Format:
+	*	delay: (ms)
+	*	dram: (joules)	core: (joules)	pkg: (joules)
+	*	dram: (joules)	core: (joules)	pkg: (joules)
+	*	dram: (joules)	core: (joules)	pkg: (joules)
+	*	dram: (joules)	core: (joules)	pkg: (joules)
+	*		... et cetera ...
+	*	@return string representation of the object
+	*/
 	public String toString()
 	{
 		String s = "";
-		s += "delay: " + delay + " milliseconds\n";
+		s += "delay: " + delay + " milliseconds\treadings: " + readings.size() + "\n";
 		for (double[] reading : readings)
 			s += labelledReading(reading) + "\n";
 		return s;
