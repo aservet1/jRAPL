3d137f5714d78e64786d7034ed63b422f30418b1
Gitea <aservet1@binghamton.edu>
2020-06-07 12:34:43 -0400

    EnergyReadingCollector now extends JRAPL

--- src/GeneralTestDriver.java
+++ src/GeneralTestDriver.java
@@ -39,7 +39,18 @@ public class GeneralTestDriver
 
 	public static void main(String[] args)
 	{
-		
+		EnergyCheckUtils.ProfileInit();
+
+		EnergyReadingCollector ec = new EnergyReadingCollector();
+
+		ec.startReading();
+		try { Thread.sleep(100); } catch (Exception e) {}
+		ec.stopReading();
+
+		System.out.println(ec);
+
+		EnergyCheckUtils.ProfileDealloc();
+			
 	}
 
 }
