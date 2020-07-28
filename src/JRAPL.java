058517c461a7e947aaa92182b32ea052e5ead06b
Alejandro Servetto <aservet1@binghamton.edu>
2020-07-28 09:43:57 -0400

    merge stuff

--- src/JRAPL.java
+++ src/JRAPL.java
@@ -3,6 +3,7 @@ package jrapl;
 import java.lang.reflect.Field;
 import java.util.Map;
 //import java.lang.invoke.MethodHandles;
+//import java.lang.invoke.VarHandle;
 
 /**
 *	Superclass for all JRAPL-using classes. Loads the native jRAPL library into memory
@@ -31,13 +32,13 @@ public class JRAPL {
 			Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
 			fieldSysPath.setAccessible(true);
 			fieldSysPath.set(null, null);
-			/*Lookup cl = MethodHandles.privateLookupIn(ClassLoader.class, MethodHandles.lookup());
+			/*MethodHandles.Lookup cl = MethodHandles.privateLookupIn(ClassLoader.class, MethodHandles.lookup());
 			VarHandle sys_paths = cl.findStaticVarHandle(ClassLoader.class, "sys_paths", String[].class);
 			sys_paths.set(null);*/
 		} catch (Exception e) { }
 
 		try {
-			NativeUtils.loadLibraryFromJar("/home/alejandro/jRAPL/jRaplSourceCode/libCPUScaler.so");
+			NativeUtils.loadLibraryFromJar("/home/alejandro/jRAPL/src/libCPUScaler.so");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
