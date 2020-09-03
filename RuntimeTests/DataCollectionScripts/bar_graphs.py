a194194208f67d5229013622d73101baca07879d
Gitea <aservet1@binghamton.edu>
2020-09-03 16:35:24 -0400

    added labels to bar graphs

--- RuntimeTests/DataCollectionScripts/bar_graphs.py
+++ RuntimeTests/DataCollectionScripts/bar_graphs.py
@@ -5,8 +5,48 @@ from statistics import mean
 from statistics import stdev
 import sys
 
+#thanks, stackoverflow :)
+def add_value_labels(ax, spacing=5):
+    """Add labels to the end of each bar in a bar chart.
 
+    Arguments:
+        ax (matplotlib.axes.Axes): The matplotlib object containing the axes
+            of the plot to annotate.
+        spacing (int): The distance between the labels and the bars.
+    """
 
+    # For each bar: Place a label
+    for rect in ax.patches:
+        # Get X and Y placement of label from rect.
+        y_value = rect.get_height()
+        x_value = rect.get_x() + rect.get_width() / 2
+
+        # Number of points between bar and label. Change to your liking.
+        space = spacing
+        # Vertical alignment for positive values
+        va = 'bottom'
+
+        # If value of bar is negative: Place label below bar
+        if y_value < 0:
+            # Invert space to place label below
+            space *= -1
+            # Vertically align label at top
+            va = 'top'
+
+        # Use Y value as label and format number with one decimal place
+        label = "{:.1f}".format(y_value)
+
+        # Create annotation
+        ax.annotate(
+            label,                      # Use `label` as label
+            (x_value, y_value),         # Place label at end of the bar
+            xytext=(0, space),          # Vertically shift label by `space`
+            textcoords="offset points", # Interpret `xytext` as offset in points
+            ha='center',                # Horizontally center label
+            va=va)                      # Vertically align label differently for
+                                        # positive and negative values
+
+'''-----------------------------------------------------------------------------------'''
 
 folders = sys.argv
 
@@ -61,4 +101,5 @@ ax.set_xticks(xticks)
 ax.set_xticklabels(statdict[keys[0]][0])
 ax.legend((p1[0], p2[0]), (keys[0], keys[1]))
 ax.autoscale_view()
+add_value_labels(ax)
 plt.savefig(keys[0] + '-' + keys[1] + '-bar_graph')
