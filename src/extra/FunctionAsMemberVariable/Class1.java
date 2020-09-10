aec4e8cf37af4865b1970b9a98c18d9cad224b70
Gitea <aservet1@binghamton.edu>
2020-09-10 13:26:29 -0400

    testing the whole function passing around / lambda expressions thing, will be a handy thing to be able to assign functions to variables

---
+++ src/extra/FunctionAsMemberVariable/Class1.java
@@ -0,0 +1,9 @@
+class Class1
+{
+
+	public static void helloworld()
+	{
+		System.out.println("hell0w@rld");
+	}
+
+}
---
+++ src/extra/FunctionAsMemberVariable/Class2.java
@@ -0,0 +1,8 @@
+class Class2
+{
+	public static int goodbyeworld()
+	{
+		System.out.println("good bye mo0n");
+		return -1;
+	}
+}
