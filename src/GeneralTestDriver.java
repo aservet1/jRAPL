a978f0584c2aa21dc50308889e23a1bd78777d29
Gitea <aservet1@binghamton.edu>
2020-09-17 10:17:24 -0400

    arr-based version that stores stats instead of diff. will now go back to the energy-manager branch with these changes to consolidate the obj and arr based gathering methods

--- src/GeneralTestDriver.java
+++ src/GeneralTestDriver.java
@@ -8,6 +8,16 @@ public class GeneralTestDriver
 {
 	public static void main(String[] args)
 	{
+		AsyncEnergyMonitorJavaSide aemonj = new AsyncEnergyMonitorJavaSide();
+		aemonj.start();
+		try { Thread.sleep(5000); }
+		catch (Exception e) {}
+		aemonj.stop();
+		System.out.println(aemonj);
+	}
+
+	private static void bothMemoryThings()
+	{
 		memoryThing("Object");
 		memoryThing("Array");
 	}
