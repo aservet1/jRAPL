81cbcd72a034e384fc474ec6ef09e2653abd5411
Alejandro Servetto <aservet1@binghamton.edu>
2020-09-22 06:00:07 -0400

    pretty much done with the info parser, still need to do the interface thing though

---
+++ UmlClassDescription/parse-uml-info.py
@@ -0,0 +1,30 @@
+
+from sys import argv,stdin
+from re import sub as regexsub
+
+if len(argv) > 1:	infile = open(argv[1],'r')
+else:			infile = stdin
+data = infile.read()
+infile.close()
+
+data = [ d.strip() for d in data.split('\n')]
+
+data = [ d.replace('native ','native_') for d in data ]
+data = [ d.replace('static ', 'static_') for d in data ]
+data = [ d.replace('final ', 'final_') for d in data ]
+data = [ d.replace('abstract ', 'abstract_') for d in data]
+data = [ regexsub('[;\{\}]','',d) for d in data ]
+
+
+publics = list(filter(lambda l: l.startswith('public'), data))
+
+publics = [ regexsub('\(.*\)','()',p) for p in publics ]
+publics = [ p.split()[1:] for p in publics ]
+
+for pub in publics:
+	if len(pub) == 1: #most likely a constructor
+		print(f"+{pub[0]}")
+	elif 'class' in pub[0]:
+		print(f"<<{' '.join(pub)}>>")
+	else:
+		print(f"+{pub[1]}: {pub[0]}")
