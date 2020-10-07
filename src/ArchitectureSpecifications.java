3302177f37baf818b60a654949284cad457d7855
Alejandro <aservet1@binghamton.edu>
2020-10-06 20:26:01 -0400

    updated todo, some touches on ArchSpec

--- src/ArchitectureSpecifications.java
+++ src/ArchitectureSpecifications.java
@@ -6,12 +6,15 @@ public final class ArchitectureSpecifications {
 	public static final boolean readingGPU;
 	public static final int NUM_SOCKETS;	
 	public static final int ENERGY_WRAP_AROUND;
+	public static final int cpuModel;
+	public static final String cpuModelName;
 
-	//public static final int numberOfPowerDomainsSupported;
 
-	public native static int GetWraparoundEnergy();
-	public native static int GetSocketNum();
 	public native static int DramOrGpu();
+	public native static int GetSocketNum();
+	public native static int GetWraparoundEnergy();
+	public native static String GetCpuModelName();
+	public native static int GetCpuModel();
 
 
 	static {
@@ -21,5 +24,6 @@ public final class ArchitectureSpecifications {
 		NUM_SOCKETS = GetSocketNum();
 		ENERGY_WRAP_AROUND = GetWraparoundEnergy();
 	}
+
 
 }
