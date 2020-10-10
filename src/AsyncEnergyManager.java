0776b964324d7dd8409432653f01acae44130ea4
Alejandro <aservet1@binghamton.edu>
2020-10-09 20:38:32 -0400

    smallfix

--- src/AsyncEnergyManager.java
+++ src/AsyncEnergyManager.java
@@ -19,6 +19,6 @@ public abstract class AsyncEnergyManager {
 	//  something about whether to return individual guys or have a whole
 	//  collection of these guys circling around in here and start/stopping
 	//  whatever many multi threaded operations are going on in here...(should learn more about in what ways one can do multi threading)
-	public abstract AsyncEnergyMonitor getMonitor(int samplingRate);
+	public abstract AsyncMonitor getMonitor(int samplingRate);
 
 }
