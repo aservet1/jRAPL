d20f3d93619560d6f1c3a8a8cb234feda8f72ae6
Alejandro <aservet1@binghamton.edu>
2020-11-08 21:42:54 -0500

    small change

--- src/SyncEnergyMonitor.java
+++ src/SyncEnergyMonitor.java
@@ -18,7 +18,7 @@ public class SyncEnergyMonitor extends EnergyMonitor {
 		super.dealloc();
 	}
 
-	public EnergyStats getObjectSample(int socket) // for a specific socket
+	public EnergyStats getObjectSample(int socket)
 	{ 
 		String energyString = EnergyMonitor.energyStatCheck(socket);
 		Instant birthday = Instant.now();
@@ -28,18 +28,6 @@ public class SyncEnergyMonitor extends EnergyMonitor {
 
 	public EnergyStats[] getObjectSample()
 	{
-		//EnergyStats[] stats = new EnergyStats[ArchSpec.NUM_SOCKETS];
-		//double[] energy = EnergyCheckUtils.getEnergyStats();
-
-		//int lo = 0;
-		//int hi = ArchSpec.NUM_STATS_PER_SOCKET;
-
-		//for (int i = 0; i < ArchSpec.NUM_SOCKETS; i++) {
-		//	int socket = i+1;
-		//	stats[i] = new EnergyStats(socket, Arrays.copyOfRange(energy,lo,hi));
-		//	lo += ArchSpec.NUM_STATS_PER_SOCKET;
-		//	hi += ArchSpec.NUM_STATS_PER_SOCKET;
-		//}
 		String energyString = EnergyMonitor.energyStatCheck(0);
 		Instant birthday = Instant.now();
 		EnergyStats[] objects = EnergyStringParser.toObjectArray(energyString);
@@ -54,15 +42,9 @@ public class SyncEnergyMonitor extends EnergyMonitor {
 	}
 	
 	public double[] getPrimitiveSample(int socket)
-	{ //TODO make a more extensive "target-one-socket-at-a-time" implementation on the C side
-	  // instead of reading all of the sockets and just picking out the ones that you want.
-	  // more efficient that way; no unnecessary sockets accessed
-		//int lo = socket-1;
-		//int hi = lo + ArchSpec.NUM_SOCKETS;
-		
+	{ 
 		String energyString = EnergyMonitor.energyStatCheck(socket);
-		//double[] energyArray = EnergyStringParser.toPrimitiveArray(energyString);
-		return EnergyStringParser.toPrimitiveArray(energyString); //Arrays.copyOfRange(energyArray,lo,hi);
+		return EnergyStringParser.toPrimitiveArray(energyString);
 	}
 
 	public static void main(String[] args) throws InterruptedException
@@ -70,19 +52,20 @@ public class SyncEnergyMonitor extends EnergyMonitor {
 		SyncEnergyMonitor monitor = new SyncEnergyMonitor();
 		monitor.init();
 
-		for (int i = 0; i < 1000; i++) {
-			double[] sample = monitor.getPrimitiveSample(1);
-			System.out.println(Arrays.toString(sample));
-			Thread.sleep(40);
+		for (int n = 0; n < 1000; n++) {
+			EnergyStats[] a = monitor.getObjectSample();
+			System.out.println(Arrays.toString(a));
+			Thread.sleep(47);
 		}
 
-		//EnergyStats before = monitor.getObjectSample(1);
+		//int socket = 1;
+		//EnergyStats before = monitor.getObjectSample(socket);
 		//EnergyStats after;
 		//EnergyDiff diff;
 		//for (int i = 0; i < 1000; i++) {
 		//	try { Thread.sleep(40); }
 		//	catch (Exception e) { e.printStackTrace(); }
-		//	after = monitor.getObjectSample(1);
+		//	after = monitor.getObjectSample(socket);
 		//	diff = EnergyDiff.between(before, after);
 		//	System.out.println(diff.dump());
 		//	before = after;
