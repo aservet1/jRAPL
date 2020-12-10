c2e9f1d5d0e2c5bf82de0b30458dfd18d43b449c
alejandro <aservet1@binghamton.edu>
2020-12-10 08:01:39 -0500

    consolidated runtime things into one subplot

--- tests/RuntimeTests/DataCollectionPrograms/create_graphs.py
+++ tests/RuntimeTests/DataCollectionPrograms/create_graphs.py
@@ -10,27 +10,48 @@ stats = False
 if "stats" in files:
     stats = True
 
-for file in files:
-    parts = file.split('.')
-    if(len(parts) == 1):
-        continue   
-    stats_fh = open('stats', 'a')
-    if(parts[1] == 'data'):
-        y = []
-        fh = open(file, 'r')
-        for datapoint in fh:
-            y.append(int(datapoint))
-        x = range(1, len(y)+1)
-        plt.scatter(x, y, marker = ",", s = 1)
-        plt.ylabel('Time (μs)')
-        plt.title(parts[0])
-        plt.xlabel('Trial number')
-        plt.savefig(parts[0]+'_scatter')
-        plt.clf()
-        if(len(y) == 0):
-            y.append(0)
-            y.append(0)
-        if not stats:
-            stats_fh.write("stats for {}:\nmean: {}\nstddev: {}\n--------------------------\n".format(parts[0], mean(y), stdev(y)))
+files = [f for f in files if len(f.split('.')) == 2 and f.split('.')[1] == 'data']
 
+if len(files) % 2 != 0:
+    rows = len(files)
+    columns = 1
+else:
+    rows = len(files) // 2
+    columns = 2 
 
+print(files)
+
+fig, axs = plt.subplots(rows, columns,  \
+            gridspec_kw={'hspace':.25} if len(files) == 3 else {'hspace':.5,'wspace':.5}, \
+            figsize=(6,4*len(files)) if len(files) == 3 else None)
+
+#print(files)
+
+i = 0
+for ax in axs.flat:
+    if (i >= len(files)): continue
+
+    stats_fh = open('stats', 'a')    
+    y = []
+    fh = open(files[i], 'r')
+    for datapoint in fh:
+        y.append(int(datapoint))
+    x = range(1, len(y)+1)
+    ax.scatter(x, y, marker = ",", s = 1)
+    ax.set_ylabel('Time (μs)')
+    name = files[i].split('.')[0]
+    ax.set_title(name)
+    ax.set_xlabel('Trial number')
+    #axs.flat[i].savefig(parts[0]+'_scatter')
+    #axs.flat[i].clf()
+    if(len(y) == 0):
+        y.append(0)
+        y.append(0)
+    if not stats:
+        stats_fh.write("stats for {}:\nmean: {}\nstddev: {}\n--------------------------\n".format(name, mean(y), stdev(y)))
+
+    i += 1
+
+#plt.figure(figsize=(6,4))
+#plt.show()
+plt.savefig('scatters')
