47012442e29fcf1804e0ade611391ce02f544418
aservet1 <aservet1@binghamton.edu>
2021-01-15 13:49:12 -0500

    native library now easily accessed in jar

--- src/main/java/jrapl/EnergyManager.java
+++ src/main/java/jrapl/EnergyManager.java
@@ -1,6 +1,7 @@
 
 package jrapl;
 
+import java.lang.reflect.Field;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -9,7 +10,7 @@ public class EnergyManager
 {
 	private static boolean modprobed = false;
 	private static boolean libraryLoaded = false;
-	private static int energyManagersActive = 0 ;
+	private static int energyManagersActive = 0; // counter for shared resource, decrease on dealloc(), when you get to 0 dealloc everything
 
 	//@TODO these should eventually be private methods
 	public native static void profileInit();
@@ -23,18 +24,18 @@ public class EnergyManager
 		String s;
 		try {
 			Process p = Runtime.getRuntime().exec(command);
-        	BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        	BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-			while ((s = stdInput.readLine()) != null) {
+        		BufferedReader stdin = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        		BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			while ((s = stdin.readLine()) != null) {
 				System.out.println(s); // printing stdout
-			} while ((s = stdError.readLine()) != null) {
+			} while ((s = stderr.readLine()) != null) {
 				System.out.println(s); // printing stderr
 			}
 		} catch (IOException e) {
-        	System.out.println("<<<IOException in execCmd():");
-        	e.printStackTrace();
-        	System.exit(-1);
-        }
+        		System.out.println("<<<IOException in execCmd():");
+        		e.printStackTrace();
+        		System.exit(-1);
+        	}
 	}
 	private static void sudoModprobeMsr() {
 		execCmd("sudo modprobe msr");
@@ -44,19 +45,19 @@ public class EnergyManager
 
 		// do NOT delete this commented sections in this !! it's going to be useful when i actually need to load library from jar
 		
-		/*try {
+		try {
 			Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
 			fieldSysPath.setAccessible(true);
 			fieldSysPath.set(null, null);
 			//MethodHandles.Lookup cl = MethodHandles.privateLookupIn(ClassLoader.class, MethodHandles.lookup());
 			//VarHandle sys_paths = cl.findStaticVarHandle(ClassLoader.class, "sys_paths", String[].class);
 			//sys_paths.set(null);
-		} catch (Exception e) { }*/	
+		} catch (Exception e) { }	
 
-		String nativelib = "/home/alejandro/jRAPL/src/main/java/jrapl/NativeLib/libCPUScaler.so";
+		String nativelib = "libCPUScaler.so";
 		try {
-			//NativeUtils.loadLibraryFromJar("libCPUScaler.so");
-			System.load(nativelib);
+			NativeUtils.loadLibraryFromJar("/NativeLib/"+nativelib);
+			//System.load(nativelib);
 		} catch (Exception e) {
 			System.err.println("ERROR LOADING LIBRARY " + nativelib);
 			e.printStackTrace();
@@ -66,9 +67,12 @@ public class EnergyManager
 	}
 	public void init() //get a better name
 	{
-		if (!modprobed) sudoModprobeMsr();
-		if (!libraryLoaded) loadLibrary();
-		if (energyManagersActive++ == 0) profileInit();
+		if (!modprobed)
+			sudoModprobeMsr();
+		if (!libraryLoaded)
+			loadLibrary();
+		if (energyManagersActive++ == 0)
+			profileInit();
 		ArchSpec.init(); // there's definitely a better way of doing this
 	}
 
--- src/main/java/jrapl/NativeUtils.java
+++ src/main/java/jrapl/NativeUtils.java
@@ -24,90 +24,120 @@
 package jrapl;
 
 import java.io.*;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.ProviderNotFoundException;
+import java.nio.file.StandardCopyOption;
 
-/** 
+/**
  * A simple library class which helps with loading dynamic libraries stored in the
- * JAR archive. These libraries usualy contain implementation of some methods in
+ * JAR archive. These libraries usually contain implementation of some methods in
  * native code (using JNI - Java Native Interface).
- *
- * @see http://adamheinrich.com/blog/2012/how-to-load-native-jni-library-from-jar
- * @see https://github.com/adamheinrich/native-utils
+ * 
+ * @see <a href="http://adamheinrich.com/blog/2012/how-to-load-native-jni-library-from-jar">http://adamheinrich.com/blog/2012/how-to-load-native-jni-library-from-jar</a>
+ * @see <a href="https://github.com/adamheinrich/native-utils">https://github.com/adamheinrich/native-utils</a>
  *
  */
 public class NativeUtils {
+ 
+    /**
+     * The minimum length a prefix for a file has to have according to {@link File#createTempFile(String, String)}}.
+     */
+    private static final int MIN_PREFIX_LENGTH = 3;
+    public static final String NATIVE_FOLDER_PATH_PREFIX = "nativeutils";
 
-    /** 
+    /**
+     * Temporary directory which will contain the DLLs.
+     */
+    private static File temporaryDir;
+
+    /**
      * Private constructor - this class will never be instanced
      */
     private NativeUtils() {
     }
 
-    /** 
+    /**
      * Loads library from current JAR archive
-     *
-     * The file from JAR is copied into system temporary directory and then loaded. The temporary file is deleted after exiting.
+     * 
+     * The file from JAR is copied into system temporary directory and then loaded. The temporary file is deleted after
+     * exiting.
      * Method uses String as filename because the pathname is "abstract", not system-dependent.
-     *
+     * 
      * @param path The path of file inside JAR as absolute path (beginning with '/'), e.g. /package/File.ext
      * @throws IOException If temporary file creation or read/write operation fails
      * @throws IllegalArgumentException If source file (param path) does not exist
-     * @throws IllegalArgumentException If the path is not absolute or if the filename is shorter than three characters (restriction of {@see File#createTempFile(java.lang.String, java.lang.String)}).
+     * @throws IllegalArgumentException If the path is not absolute or if the filename is shorter than three characters
+     * (restriction of {@link File#createTempFile(java.lang.String, java.lang.String)}).
+     * @throws FileNotFoundException If the file could not be found inside the JAR.
      */
     public static void loadLibraryFromJar(String path) throws IOException {
-        if (!path.startsWith("/")) {
+ 
+        if (null == path || !path.startsWith("/")) {
             throw new IllegalArgumentException("The path has to be absolute (start with '/').");
         }
-
+ 
         // Obtain filename from path
         String[] parts = path.split("/");
         String filename = (parts.length > 1) ? parts[parts.length - 1] : null;
-
-        // Split filename to prexif and suffix (extension)
-        String prefix = "";
-        String suffix = null;
-        if (filename != null) {
-            parts = filename.split("\\.", 2);
-            prefix = parts[0];
-            suffix = (parts.length > 1) ? "."+parts[parts.length - 1] : null; // Thanks, davs! :-)
-        }
-
+ 
         // Check if the filename is okay
-        if (filename == null || prefix.length() < 3) {
+        if (filename == null || filename.length() < MIN_PREFIX_LENGTH) {
             throw new IllegalArgumentException("The filename has to be at least 3 characters long.");
         }
-
+ 
         // Prepare temporary file
-        File temp = File.createTempFile(prefix, suffix);
-        temp.deleteOnExit();
-
-        if (!temp.exists()) {
-            throw new FileNotFoundException("File " + temp.getAbsolutePath() + " does not exist.");
+        if (temporaryDir == null) {
+            temporaryDir = createTempDirectory(NATIVE_FOLDER_PATH_PREFIX);
+            temporaryDir.deleteOnExit();
         }
 
-        // Prepare buffer for data copying
-        byte[] buffer = new byte[1024];
-        int readBytes;
+        File temp = new File(temporaryDir, filename);
 
-        // Open and check input stream
-        InputStream is = NativeUtils.class.getResourceAsStream(filename);
-
-        if (is == null) {
+        try (InputStream is = NativeUtils.class.getResourceAsStream(path)) {
+            Files.copy(is, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            temp.delete();
+            throw e;
+        } catch (NullPointerException e) {
+            temp.delete();
             throw new FileNotFoundException("File " + path + " was not found inside JAR.");
         }
 
-        // Open output stream and copy data between source file in JAR and the temporary file
-        OutputStream os = new FileOutputStream(temp);
         try {
-            while ((readBytes = is.read(buffer)) != -1) {
-                os.write(buffer, 0, readBytes);
-            }
+            System.load(temp.getAbsolutePath());
         } finally {
-            // If read/write fails, close streams safely before throwing an exception
-            os.close();
-            is.close();
+            if (isPosixCompliant()) {
+                // Assume POSIX compliant file system, can be deleted after loading
+                temp.delete();
+            } else {
+                // Assume non-POSIX, and don't delete until last file descriptor closed
+                temp.deleteOnExit();
+            }
         }
+    }
 
-        // Finally, load the library
-        System.load(temp.getAbsolutePath());
+    private static boolean isPosixCompliant() {
+        try {
+            return FileSystems.getDefault()
+                    .supportedFileAttributeViews()
+                    .contains("posix");
+        } catch (FileSystemNotFoundException
+                | ProviderNotFoundException
+                | SecurityException e) {
+            return false;
+        }
+    }
+
+    private static File createTempDirectory(String prefix) throws IOException {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        File generatedDir = new File(tempDir, prefix + System.nanoTime());
+        
+        if (!generatedDir.mkdir())
+            throw new IOException("Failed to create temp directory " + generatedDir.getName());
+        
+        return generatedDir;
     }
 }
+
