6d0b9603ab11b2253ac976c84154d02f33a90b99
alejandro <aservet1@binghamton.edu>
2021-02-01 02:47:54 -0500

    aesthetic change

--- src/main/java/jRAPL/EnergyMonitor.java
+++ src/main/java/jRAPL/EnergyMonitor.java
@@ -2,13 +2,11 @@ package jRAPL;
 
 public class EnergyMonitor extends EnergyManager {
 
-	public void init() {
-		super.init();
-	}
+	@Override
+	public void init() { super.init(); }
 
-	public void dealloc() {
-		super.dealloc();
-	}
+	@Override
+	public void dealloc() { super.dealloc(); }
 
 	// whichSocket = 0 for all sockets	
 	// package private so it can be called in JMH things
