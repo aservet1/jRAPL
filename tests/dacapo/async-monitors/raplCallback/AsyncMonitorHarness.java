536a5447823875c32a345e5bf8aa968641f3d7e4
alejandro <aservet1@binghamton.edu>
2021-03-02 00:01:49 -0500

    made callback work, now i have to run it how its supposed to be

--- tests/dacapo/async-monitors/raplCallback/AsyncMonitorHarness.java
+++ tests/dacapo/async-monitors/raplCallback/AsyncMonitorHarness.java
@@ -1,9 +1,12 @@
 
+package raplCallback;
+
 import org.dacapo.harness.Callback;      
 import org.dacapo.harness.CommandLineArgs;   
 import java.io.*;
 
 import jRAPL.AsyncEnergyMonitor;
+import jRAPL.AsyncEnergyMonitorCSide;
 import jRAPL.AsyncEnergyMonitorJavaSide;
 
 public class AsyncMonitorHarness extends Callback {
@@ -11,17 +14,13 @@ public class AsyncMonitorHarness extends Callback {
 	public static int MAX_ITERATIONS = 20;
 	public static int CURRENT_ITERATION = 1;
 	
-	public static long[] START_ITER_TS = new long[MAX_ITERATIONS];
-	public static long[] STOP_ITER_TS = new long[MAX_ITERATIONS];
-	
-	private static final int FIRE_AFTER = 5;
+	private static final int FIRE_AFTER = 2;
 
 	AsyncEnergyMonitor m;
 	
 	public AsyncMonitorHarness(CommandLineArgs args) {
 		super(args);
-		m = new AsyncEnergyMonitorJavaSide();
-		m.activate();
+		m = new AsyncEnergyMonitorCSide();
 	}
 
 	@Override
@@ -29,12 +28,11 @@ public class AsyncMonitorHarness extends Callback {
 		super.stop(l, w);
 		m.stop();	
 		
-		STOP_ITER_TS[CURRENT_ITERATION-1] = System.currentTimeMillis();
-		System.out.println("Iteration "+CURRENT_ITERATION+" Stopping");
 		CURRENT_ITERATION++;
 		
 		if (CURRENT_ITERATION > FIRE_AFTER) {
 			System.out.println(m);
+			m.writeToFile(CURRENT_ITERATION+".log");
 		}
 		
 		m.reset();
@@ -43,23 +41,25 @@ public class AsyncMonitorHarness extends Callback {
 	@Override
 	public void start(String benchmark) {
 		super.start(benchmark);
+		m.activate();
+		m.setSamplingRate(0);
 		m.start();
-		START_ITER_TS[CURRENT_ITERATION-1] = System.currentTimeMillis();
 	}
 
 	@Override
 	public void complete(String benchmark, boolean valid) {
-		try {
-			FileWriter fileWriter = new FileWriter("iteration_times");
-			PrintWriter printWriter = new PrintWriter(fileWriter);
-			for(int i = FIRE_AFTER; i <= CURRENT_ITERATION; i++) {
-				printWriter.printf("%d\n",STOP_ITER_TS[i],START_ITER_TS[i]);
-			}
-			printWriter.close();
-			
-		} catch(Exception exception) {
-			System.out.println(exception.getMessage());
-		}
+		super.complete(benchmark, valid);
+		// try {
+		// 	FileWriter fileWriter = new FileWriter("iteration_times");
+		// 	PrintWriter printWriter = new PrintWriter(fileWriter);
+		// 	for(int i = FIRE_AFTER; i <= CURRENT_ITERATION; i++) {
+		// 		printWriter.printf("%d\n",STOP_ITER_TS[i],START_ITER_TS[i]);
+		// 	}
+		// 	printWriter.close();
+		// 	
+		// } catch(Exception exception) {
+		// 	System.out.println(exception.getMessage());
+		// }
 		m.deactivate();
 	}
 
---
+++ tests/dacapo/async-monitors/run.sh
@@ -0,0 +1,3 @@
+cmd='sudo java -cp dacapo-evaluation-git+309e1fa.jar:jRAPL-1.0.jar:raplCallback Harness sunflow -c AsyncMonitorHarness -n 3'
+echo $cmd
+eval $cmd
