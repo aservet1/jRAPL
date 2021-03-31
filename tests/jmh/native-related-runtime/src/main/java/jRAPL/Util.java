993cb718ec18924c3ebac22690ef0091e14a1c61
Rutvik <rutvik20007@gmail.com>
2021-03-30 20:37:21 -0400

    Forgot to initialize i

--- tests/jmh/native-related-runtime/src/main/java/jRAPL/Util.java
+++ tests/jmh/native-related-runtime/src/main/java/jRAPL/Util.java
@@ -4,6 +4,7 @@ import org.openjdk.jmh.infra.Blackhole;
 
 public class Util {
     public static void busyWait(Blackhole b) {
+        int i = 0;
         /* Busy wait so that we dont break the msrs*/
         while(i < 100) b.consume(i++);
     }
