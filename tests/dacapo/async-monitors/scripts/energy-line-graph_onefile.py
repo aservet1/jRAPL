f89bb6154eb3805627701204e6fca9cf81f371e5
alejandro <aservet1@binghamton.edu>
2021-05-20 14:53:30 -0400

    much updating of scripts

--- tests/dacapo/async-monitors/scripts/energy-line-graph_onefile.py
+++ tests/dacapo/async-monitors/scripts/energy-line-graph_onefile.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-import json
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 from sys import argv
 
 def diff_list(l):
@@ -11,8 +9,7 @@ def diff_list(l):
     for i in range(1,len(l)):
         d.append(l[i]-l[i-1])
     return d
-def values_list(dictionary):
-	return list(dictionary.values())
+
 def filenamestub(path):
 	return path.split('/')[-1].split('.')[0]
 
@@ -23,24 +20,13 @@ if not filename.endswith('.csv'):
 
 df = pd.read_csv(filename)
 del df['core'] #hardcoded delete core bc core doesnt work on jolteon
+del df['socket'] #assume that socket has already been filtered before the file is passed in here
 
-data = dict()
-num_sockets = max(df['socket'])
-for i in range(num_sockets): # filling out the socket level of result{}
-    socket = i+1
-    current = df[df.socket.eq(socket)].to_dict()
-    del current['socket']
-    data[socket] = current
-    
-for socket in data:
-    for powd in data[socket]:
-        #y = [ data[socket][powd][i] for i in data[socket][powd] ]
-        y = values_list(data[socket][powd])
-        y = diff_list(y)
-        #x = list(range(1,len(y)+1))
-        x = diff_list(values_list(data[socket]['timestamp']))
-        plt.plot(x,y)
-        print(len(x),len(y))
-
-plt.savefig('EnergySeries_'+filenamestub(filename))
-plt.clf()
+times = df['timestamp']
+del df['timestamp']
+for powd in df:
+    energy = df[powd].to_list()
+    x = times; y = energy
+    plt.plot(x,y)
+    plt.savefig('EnergySeries_'+powd+'_'+filenamestub(filename))
+    plt.clf()
--- tests/dacapo/async-monitors/scripts/vis_res_memoryconsume.py
+++ tests/dacapo/async-monitors/scripts/vis_res_memoryconsume.py
@@ -32,35 +32,39 @@ for d in data:
 data = x
 
 labels = []
+
 java_avg = []
 c_da_avg = []
-
 c_ll_avg = []
-java_std = []
+nojrapl_avg = []
 
+java_std = []
 c_da_std = []
 c_ll_std = []
-
-nojrapl_avg = []
 nojrapl_std = []
 
-def get_by_monitor_type(data, monitor_type):
-    return [ d for d in data[benchmark] if d['metadata']['monitor_type'] == monitor_type ][0]['memory']
+java_numsamples = []
+c_ll_numsamples = []
+c_da_numsamples = []
 
 for benchmark in data:
     if benchmark == 'h2': continue
 
-    javg = get_by_monitor_type(data, 'java')['jraplon']['avg']
-    jstd = get_by_monitor_type(data, 'java')['jraplon']['stdev']
+    def get_by_monitor_type(data, monitor_type):
+        return [ d for d in data[benchmark] if d['metadata']['monitor_type'] == monitor_type ][0]
 
-    clavg = get_by_monitor_type(data, 'c-linklist')['jraplon']['avg'] 
-    clstd = get_by_monitor_type(data, 'c-linklist')['jraplon']['stdev']
+    javg = get_by_monitor_type(data, 'java')['memory']['jraplon']['avg']
+    jstd = get_by_monitor_type(data, 'java')['memory']['jraplon']['stdev']
 
-    cdavg = get_by_monitor_type(data, 'c-dynamicarray')['jraplon']['avg']  
-    cdstd = get_by_monitor_type(data, 'c-dynamicarray')['jraplon']['stdev']
+    clavg = get_by_monitor_type(data, 'c-linklist')['memory']['jraplon']['avg'] 
+    clstd = get_by_monitor_type(data, 'c-linklist')['memory']['jraplon']['stdev']
 
-    nojavg = get_by_monitor_type(data, 'c-dynamicarray')['jraploff']['avg']
-    nojstd = get_by_monitor_type(data, 'c-dynamicarray')['jraploff']['stdev']
+    cdavg = get_by_monitor_type(data, 'c-dynamicarray')['memory']['jraplon']['avg']  
+    cdstd = get_by_monitor_type(data, 'c-dynamicarray')['memory']['jraplon']['stdev']
+
+    nojavg = get_by_monitor_type(data, 'c-dynamicarray')['memory']['jraploff']['avg']
+    nojstd = get_by_monitor_type(data, 'c-dynamicarray')['memory']['jraploff']['stdev']
+
 
     def percent_diff(a,b):
         return (a-b)/((a+b)/2)
@@ -77,24 +81,27 @@ for benchmark in data:
 
     java_avg.append( percent_diff(javg, nojavg) )
     java_std.append( percent_diff_stdev(jstd, nojstd, javg, nojavg) )
+    java_numsamples.append( get_by_monitor_type(data, 'java')['metadata']['numSamples'] )
 
     c_ll_avg.append( percent_diff(clavg, nojavg) )
     c_ll_std.append( percent_diff_stdev(clstd, nojstd, clavg, nojavg) )
+    c_ll_numsamples.append( get_by_monitor_type(data, 'c-linklist')['metadata']['numSamples'] )
 
     c_da_avg.append( percent_diff(cdavg, nojavg) )
     c_da_std.append( percent_diff_stdev(cdavg, nojavg, cdstd, nojstd) )
+    c_da_numsamples.append( get_by_monitor_type(data, 'c-dynamicarray')['metadata']['numSamples'] )
 
+## Make the all-benchmarks graph ##
 bar_width = 0.25
 mpl.rcParams['figure.dpi'] = 600
 r1 = np.arange(len(c_ll_avg))
 r2 = [x + bar_width for x in r1]
 r3 = [x + bar_width for x in r2]
-#r4 = [x + bar_width for x in r3]
+
+plt.clf()
 plt.barh(r1, c_da_avg, bar_width, xerr=c_da_std, color='#003f5c', edgecolor="white", label='C Dynamic Array')
 plt.barh(r2, c_ll_avg, bar_width, xerr=c_ll_std, color='#bc5090', edgecolor="white", label='C Linked List')
 plt.barh(r3, java_avg, bar_width, xerr=java_std, color='#ffa600', edgecolor="white", label='Java')
-#plt.barh(r4, nojrapl_avg, bar_width, xerr=nojrapl_std, color='#000000', edgecolor="white", label='No jRAPL')
-
 
 plt.ylabel('Benchmark', fontweight='bold')
 plt.xlabel('Percent Difference', fontweight='bold')
@@ -103,18 +110,44 @@ plt.legend()
 fig = plt.gcf()
 fig.set_size_inches(12,25)
 #plt.show()
-plt.savefig('memory-comparison-bar')
+plt.savefig('memory-compare_perbenchmark')
 
-## Now to average across all benchmarks and make a bar graph with error bars of the 3
+## Now to average across all benchmarks and make a bar graph with error bars of the 3 ##
+import math
 
+def aggr_mean(sample_sizes, averages):
+    assert len(sample_sizes) == len(averages)
+    return sum([ (sample_sizes[i]*averages[i]) for i in range(len(sample_sizes)) ]) / sum(sample_sizes)
 
+def aggr_stdev(sample_sizes, stdevs):
+    assert len(sample_sizes) == len(stdevs)
+    return math.sqrt(sum([ (sample_sizes[i]*(stdevs[i]**2)) for i in range(len(sample_sizes)) ]) / sum (sample_sizes))
 
+overall_java_avg = aggr_mean (java_numsamples, java_avg)
+overall_java_std = aggr_stdev(java_numsamples, java_std)
 
+overall_c_ll_avg = aggr_mean (c_ll_numsamples, c_ll_avg)
+overall_c_ll_std = aggr_stdev(c_ll_numsamples, c_ll_std)
 
+overall_c_da_avg = aggr_mean (c_da_numsamples, c_da_avg)
+overall_c_da_std = aggr_stdev(c_da_numsamples, c_da_std)
 
+labels = ['java','c-linklist','c-dynamicarray']
 
+plt.clf()
+plt.bar(x=[0,1,2], \
+    height=[overall_java_avg, overall_c_ll_avg, overall_c_da_avg], \
+    yerr=[overall_java_std, overall_c_ll_std, overall_c_da_std], \
+    tick_label=labels \
+)
 
+plt.xlabel('monitor type')
+plt.ylabel('average memory percent difference')
+plt.title('% memory difference that each type of monitor added')
 
+fig = plt.gcf()
+fig.set_size_inches(5,5)
 
-
+#plt.show()
+plt.savefig('memory-compare_overall')
 
