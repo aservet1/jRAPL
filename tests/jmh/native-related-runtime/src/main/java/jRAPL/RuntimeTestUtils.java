e1fecbb36c09fdb874e223b457c31a5ec7b5f050
alejandro <aservet1@binghamton.edu>
2021-06-15 12:27:35 -0400

    restore missing file

--- tests/jmh/native-related-runtime/src/main/java/jRAPL/RuntimeTestUtils.java
+++ tests/jmh/native-related-runtime/src/main/java/jRAPL/RuntimeTestUtils.java
@@ -5,17 +5,18 @@ import java.text.ParseException;
 import java.time.Duration;
 import java.time.Instant;
 
-/** Access to utilities I set up for native C side timing */
 public class RuntimeTestUtils
 {
-	static int DRAM  = 1, GPU = 2, CORE = 3, PKG = 4; // constants that align with macros defined on the C side
+	// constants that align with macros defined on the C side,
+	// passed in to usecTimeMSRRead
+	static final int DRAM = 1, GPU = 2, CORE = 3, PKG = 4;
 
 	/** Allocs relevant C side memory and sets up variables */
 	public native static void initCSideTiming();
 	/** Deallocs relevant C side memory */
 	public native static void deallocCSideTiming();
 
-	//Runs each function once and returns microsecond duration
+	//Runs the function in question once and returns usec runtime
 	public native static long usecTimeProfileInit();
 	public native static long usecTimeEnergyStatCheck();
 	public native static long usecTimeProfileDealloc();
