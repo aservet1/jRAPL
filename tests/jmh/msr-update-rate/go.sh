e3e7650c63497574cfe4fbd496cd5a8117ac22ea
alejandro <aservet1@binghamton.edu>
2021-07-03 18:04:40 -0400

    outputfile gets labeled by hostname

--- tests/jmh/msr-update-rate/go.sh
+++ tests/jmh/msr-update-rate/go.sh
@@ -4,5 +4,5 @@ set -e
 sudo -v
 
 mvn clean install
-sudo java -jar target/benchmarks.jar -rf json
+sudo java -DhostName=$(hostname) -jar target/benchmarks.jar -rf json
 sudo mv jmh-result.json results/last-jmh-output_$(hostname).json
