1dee7c96f0eeaffa228f7dd239428d290e63406f
alejandro <aservet1@binghamton.edu>
2021-07-07 09:29:50 -0400

    remove commented out stuff

--- tests/dacapo/async-monitors/scripts/analysis/aggregate-permonitor.py
+++ tests/dacapo/async-monitors/scripts/analysis/aggregate-permonitor.py
@@ -23,6 +23,7 @@ except:
     exit(2)
 
 os.chdir(data_directory)
+
 monitor_types = sorted(list(set([ extract_monitortype(fname) for fname in os.listdir() if fname.endswith(input_extension) ])))
 if not len(monitor_types):
     print("no files found with extension",input_extension)
@@ -34,24 +35,14 @@ for monitor_type in monitor_types:
     data = [ json.load(fd) for fd    in fds       ]
     fds  = [ fd.close()    for fd    in fds       ]
 
-    # data = list()
-    # for fname in filenames:
-    #     with open(fname) as fh:
-    #         data.append(json.loads(fh.read()))
-
-        # fhs = [open(f) for f in filenames]
-        # data = [json.loads(fh.read()) for fh in fhs]
-        # data = [d for d in data if d['metadata']['monitor_type'] == monitor_type ]
-        # for fh in fhs: fh.close()
-
     aggregated = {}
 
-    aggregated['metadata'] = data[0]['metadata'] # copy over the first [metadata] block to keep common fields, over-write the aggregated fields
+    aggregated['metadata'] = data[0]['metadata'] # copy over the first [metadata] block to keep common fields, over-write the aggregate fields
     for k in ['lifetime','numSamples']:
         dat = [ d['metadata'][k] for d in data ]
         aggregated['metadata'][k] = dict()
-        aggregated['metadata'][k]['avg']   = statistics.mean(dat)  # = aggr_mean([1 for _ in range(len(metadata))],    [d['avg'] for d in metadata])
-        aggregated['metadata'][k]['stdev'] = statistics.stdev(dat) # = aggr_stdev([1 for _ in range(len(metadata))], [d['stdev'] for d in metadata])
+        aggregated['metadata'][k]['avg']   = statistics.mean(dat)
+        aggregated['metadata'][k]['stdev'] = statistics.stdev(dat)
     aggregated['metadata']['iteration'] = 'AGGREGATE_PERMONITOR'
     aggregated['metadata']['benchmark'] = 'AGGREGATE_PERMONITOR'
 
