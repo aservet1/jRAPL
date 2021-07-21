e1d71aa74e476464bba9e77fed1af2101301577b
alejandro <aservet1@binghamton.edu>
2021-07-21 10:42:09 -0400

    more iterations

--- tests/jmh/sleep-timing/src/main/java/sleeping/Sleeping.java
+++ tests/jmh/sleep-timing/src/main/java/sleeping/Sleeping.java
@@ -77,34 +77,36 @@ public class Sleeping {
 		}
 	}
 
-	//@Benchmark
-	//@Fork(1)
-	//// @Warmup(iterations = 2)
-	//// @Measurement(iterations = 3)
-	//@Warmup(iterations = 5)
-	//@Measurement(iterations = 10)
-	//@BenchmarkMode(Mode.AverageTime)
-	//@OutputTimeUnit(TimeUnit.MICROSECONDS)
-	//public void timeJSleep(S s) throws InterruptedException {
-	//	Thread.sleep(s.TIME);
-	//}
+	// @Benchmark
+	// @Fork(1)
+	// // @Warmup(iterations = 2)
+	// // @Measurement(iterations = 3)
+	// @Warmup(iterations = 5)
+	// @Measurement(iterations = 10)
+	// @BenchmarkMode(Mode.AverageTime)
+	// @OutputTimeUnit(TimeUnit.MICROSECONDS)
+	// public void timeJSleep(S s) throws InterruptedException {
+	// 	Thread.sleep(s.TIME);
+	// }
+
 	@Benchmark
 	@Fork(1)
 	// @Warmup(iterations = 2)
 	// @Measurement(iterations = 3)
 	@Warmup(iterations = 5)
-	@Measurement(iterations = 10)
+	@Measurement(iterations = 20)
 	@BenchmarkMode(Mode.AverageTime)
 	@OutputTimeUnit(TimeUnit.MICROSECONDS)
 	public void timeCSleep(S s) throws InterruptedException {
 		cSleep(s.TIME);
 	}
+
 	@Benchmark
 	@Fork(1)
 	// @Warmup(iterations = 2)
 	// @Measurement(iterations = 3)
 	@Warmup(iterations = 5)
-	@Measurement(iterations = 10)
+	@Measurement(iterations = 20)
 	@BenchmarkMode(Mode.AverageTime)
 	@OutputTimeUnit(TimeUnit.MICROSECONDS)
 	public void timeCSleepWithC(S s) throws InterruptedException {
