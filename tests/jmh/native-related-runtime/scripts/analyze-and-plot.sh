1d44a8b00a689ce1b105a5f5fb6179cef02f76e8
alejandro <aservet1@binghamton.edu>
2021-07-21 11:05:44 -0400

    update script

--- tests/jmh/native-related-runtime/scripts/analyze-and-plot.sh
+++ tests/jmh/native-related-runtime/scripts/analyze-and-plot.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ $1 != 'JavaSide' ] && [ $1 != 'CSide' ] && [ $1 != 'readMSR' ]
-then
+function usage() {
 	echo "invalid option: $1"
 	echo "must be JavaSide, CSide, or readMSR"
 	exit 2
-fi
+}
+
+[ -z $1 ] && usage
+[ $1 != 'JavaSide' ] && [ $1 != 'CSide' ] && [ $1 != 'readMSR' ] && usage
 
 files=$1_*_histogram.data
-python3 scripts/make_histogram.py $files #"$1_*_histogram.data"
+python3 scripts/make_histogram.py $files
