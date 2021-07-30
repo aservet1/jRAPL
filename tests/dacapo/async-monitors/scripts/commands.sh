59d19afb9965525fab64f0a2e7cbe142874dd9e8
Alejandro Servetto <aservet1@binghamton.edu>
2021-07-30 00:55:36 -0400

    rebuilt graphs

--- tests/dacapo/async-monitors/scripts/commands.sh
+++ tests/dacapo/async-monitors/scripts/commands.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+function echo_eval() {
+	printf "\033[0;32m" "$@" "\033[0m \n" && eval "$@"
+}
+
 set -e
 
-./scripts/analysis/memory-footprint.py jolteon-results-30/ results/memory-footprint/
-./scripts/visualization/memory-footprint.py results/memory-footprint/memory-footprint.json results/memory-footprint/
+echo_eval ./scripts/analysis/memory-footprint.py jolteon-results-30/ results/memory-footprint/
+echo_eval ./scripts/visualization/memory-footprint.py results/memory-footprint/memory-footprint.json results/memory-footprint/
 
-./scripts/analysis/energy-per-sample_timenormalized.py jolteon-results-30/ results/energy-per-sample_timenormalized/
-./scripts/visualization/energy-per-sample.py results/energy-per-sample_timenormalized/energy-per-sample.json results/energy-per-sample_timenormalized/
+echo_eval ./scripts/analysis/energy-per-sample_timenormalized.py jolteon-results-30/ results/energy-per-sample_timenormalized/
+echo_eval ./scripts/visualization/energy-per-sample.py results/energy-per-sample_timenormalized/energy-per-sample.json results/energy-per-sample_timenormalized/
 
-./scripts/analysis/energy-per-sample.py jolteon-results-30/ results/energy-per-sample/
-./scripts/visualization/energy-per-sample.py results/energy-per-sample/energy-per-sample.json results/energy-per-sample/
+echo_eval ./scripts/analysis/energy-per-sample.py jolteon-results-30/ results/energy-per-sample/
+echo_eval ./scripts/visualization/energy-per-sample.py results/energy-per-sample/energy-per-sample.json results/energy-per-sample/
 
-./scripts/analysis/time-per-sample.py jolteon-results-30/ results/time-per-sample/
-./scripts/visualization/time-per-sample.py results/time-per-sample/time-per-sample.json results/time-per-sample/
+echo_eval ./scripts/analysis/time-per-sample.py jolteon-results-30/ results/time-per-sample/
+echo_eval ./scripts/visualization/time-per-sample.py results/time-per-sample/time-per-sample.json results/time-per-sample/
 
-./scripts/analysis/sampling-efficiency.py jolteon-results-30/ results/sampling-efficiency/
-./scripts/visualization/sampling-efficiency.py results/sampling-efficiency/sampling-efficiency.json results/sampling-efficiency/
+echo_eval ./scripts/analysis/sampling-efficiency.py jolteon-results-30/ results/sampling-efficiency/
+echo_eval ./scripts/visualization/sampling-efficiency.py results/sampling-efficiency/sampling-efficiency.json results/sampling-efficiency/
