5c4299abf5ba047be794d0873fcd958a2647c13d
alejandro <aservet1@binghamton.edu>
2021-08-02 11:07:05 -0400

    doesnt autmoatically delete result dir, informs user that it exists and allows them to decide. also start and stop messages to the log

--- tests/dacapo/async-monitors/scripts/standard-experiment-run.sh
+++ tests/dacapo/async-monitors/scripts/standard-experiment-run.sh
@@ -6,19 +6,28 @@ function usage() {
 	echo "usage: $1 <result directory>"
 	exit 1
 }
+
 [ $# != 1 ] && usage $0
+
+echo " (||(.. Started on $(date)"
 
 sudo -v
 sudo echo 'hello w0rld :))'
-
-make clean all
 
 warmups=5
 iterations=30
 samplingRate=1
 resultDir=$1
 
-rm -rf $resultDir && mkdir $resultDir
+if [ -d $resultDir ]; then
+	echo " ERROR: directory '$resultDir' already exists."
+	echo "    please pick another name, or manually delete it (and be sure that you want to delete it AND that it's the one you actually want to delete / not a typo)"
+	exit 1
+fi
+mkdir $resultDir
+sudo ps -ef > $resultDir/SYSTEM_PROCESSES_THAT_WERE_RUNNING.txt
+
+make clean all
 
 benchmarks=$(sed 's/#.*$//g' benchmarks.config)
 for benchmark in $benchmarks
@@ -36,6 +45,8 @@ do
 		$warmups no-monitor $resultDir
 	
 done
+
+echo " (||(.. Completed on $(date)"
 
 # benchmarks=$(sed 's/#.*$//g' benchmarks.txt)
 # for benchmark in $benchmarks
