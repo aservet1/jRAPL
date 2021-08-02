e6ebfb411b7ccfbd3311f520c50ef327b5a0f674
Alejandro Servetto <aservet1@binghamton.edu>
2021-08-02 17:24:19 -0400

    negs counter

---
+++ tests/dacapo/async-monitors/scripts/analysis/number-of-negatives.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import json
+from sys import argv
+
+data_dir = argv[1]
+os.chdir(data_dir)
+
+caught_nefs = {
+	'negs': 0,
+	'nonnegs': 0
+}
+
+for filename in sorted([
+	filename for filename in os.listdir()
+	if filename.endswith('.memory.json')
+]):
+	print('>>','processing',filename,'...')
+	with open(filename) as fd:
+		data = json.load(fd)
+		count['negs'] += len(data['negatives_caught']['samples'])
+		count['nonnegs'] += len(data['samples'])
+
+print(count)
+print( count['negs'] / count['nonnegs'] * 100 , '%' )
