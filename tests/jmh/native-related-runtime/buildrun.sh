861fffb5f83bb71037fbf1491abefc8a0846f68f
alejandro <aservet1@binghamton.edu>
2021-08-03 20:03:55 -0400

    reorganize

--- tests/jmh/native-related-runtime/buildrun.sh
+++ tests/jmh/native-related-runtime/buildrun.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-#ignoreLock=-Djmh.ignoreLock=true
-
 if [ -z $1 ]
 then
-	echo "usage $0 data_dir"
+	echo "usage $0 result_dir"
 	exit 2
 elif [ -d $1 ]
 then
 	echo "$1 already exists."
 	exit 1
 else
-	data_dir=$1
+	result_dir=$1
 fi
 
-set -e
-sudo -v
-outputlog=$data_dir/output.log
+set -e; sudo -v
 
-mvn clean install && ./scripts/transferjars.sh jRAPL-1.0.jar target/benchmarks.jar
+outputlog=$result_dir/output.log
 
-mkdir -p $data_dir
-sudo java -DdataDir=$data_dir -jar $ignoreLock target/benchmarks.jar | tee $outputlog
+mvn clean install
+
+./scripts/transferjars.sh \
+	jRAPL-1.0.jar \
+	target/benchmarks.jar
+
+mkdir -p $result_dir
+sudo java \
+	-DdataDir=$result_dir \
+	-jar target/benchmarks.jar \
+	| tee $outputlog
