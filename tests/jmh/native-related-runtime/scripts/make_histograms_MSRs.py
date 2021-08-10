662c40caf7ecc399a912ee1349bfb182f52c9360
alejandro <aservet1@binghamton.edu>
2021-08-10 15:57:27 -0400

    made the subplots well!

--- tests/jmh/native-related-runtime/scripts/make_histograms_MSRs.py
+++ tests/jmh/native-related-runtime/scripts/make_histograms_MSRs.py
@@ -4,33 +4,12 @@ from sys import argv
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-def trunc_str(n): # decimal tostring, truncated
-    return "%.4f" % n
+'''------------------------------------------------------------------------------'''
 
-try:
-    data_files = argv[1:-1]
-    output_dir = argv[-1]
-except IndexError:
-    print("usage:",argv[0],"<list of data files> output_dir")
-    exit(2)
-if not len(data_files):
-    print("usage:",argv[0],"<list of data files> output_dir")
-    exit(2)
+# def trunc_str(n): # decimal tostring, truncated
+#     return "%.4f" % n
 
-if len(data_files) % 3 != 0:
-    print(data_files,len(data_files))
-    print("error: this is going to be an _ x 3 subplot figure, so each group of 3 data files must correspond to a row of the subplots")
-    exit(1)
-subplot_rows = len(data_files) // 3
-subplot_cols = 3
-
-clr = 'mediumslateblue'
-edgeclr = 'black'
-a = 1
-
-fig, axs = plt.subplots( subplot_rows, subplot_cols, figsize=(3,2) )
-for i, fname in enumerate( data_files ):
-    ax = axs.flat[i]
+def gather_histogram_info(fname):
     data = {}
     with open(fname) as fh:
         n = 0
@@ -53,25 +32,61 @@ for i, fname in enumerate( data_files ):
             else:
                 outliers[time] = data[time]
 
-    # print(fname)
-    # print(filtered.keys(), filtered.values())
-    # print(outliers.keys(), outliers.values())
+    return filtered, outliers, mean, sd
 
-    ax.bar(list(filtered.keys()), filtered.values(), color = clr, edgecolor = edgeclr, alpha = a)
-    extra1 = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)
-    extra2 = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)
-    ax.legend()
-    title = fname.split('.')[0].split('/')[-1].split('_')[1]
-    ax.set_title(title)
-    ax.legend([extra1, extra2], ( "x̄: "+trunc_str(mean)+"µs", "σ: "+trunc_str(sd)+"µ" ))
+'''------------------------------------------------------------------------------'''
 
-# fig.supxlabel("microseconds".title())
-# fig.supylabel("frequency".title())
+try:
+    data_files = argv[1:-1]
+    output_dir = argv[-1]
+except IndexError:
+    print("usage:",argv[0],"<list of data files> output_dir")
+    exit(2)
+if not len(data_files):
+    print("usage:",argv[0],"<list of data files> output_dir")
+    exit(2)
+
+if len(data_files) % 3 != 0:
+    print(data_files,len(data_files))
+    print("error: this is going to be an _ x 3 subplot figure, so each group of 3 data files must correspond to a row of the subplots")
+    print('  (you still might get index out of bounds errors if you pass in the wrong number of things, this is a personal script not a fooolproof consumer grade software piece. think about why that happened if you get the index error)')
+    exit(1)
+
+
+fig = plt.figure(constrained_layout=True)
+i = 0
+# create 3x1 subfigs (credit to: https://stackoverflow.com/questions/27426668/row-titles-for-matplotlib-subplot)
+row_names = ['System A', 'System B']
+subfigs = fig.subfigures (
+    nrows=len(row_names),
+    ncols=1
+)
+for row, subfig in enumerate(subfigs):
+    subfig.suptitle(row_names[row])
+
+    # create 1x3 subplots per subfig
+    axs = subfig.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
+    for col, ax in enumerate(axs):
+        fname = data_files[i]
+        filtered, outliers, mean, sd = gather_histogram_info(fname); i+=1
+        ax.bar (
+            list(filtered.keys()),
+            filtered.values(),
+            color = 'mediumslateblue',
+            edgecolor = 'black',
+            alpha = 1
+        )
+        title = fname.split('.')[0].split('/')[-1].split('_')[1]
+        ax.set_title(title)#f'Plot title {col}')
+
+fig.supylabel('Frequency')
+fig.supxlabel('Run Time (usec)')
+
 plt.show()
 
 fig.savefig (
 	os.path.join (
 		output_dir,
-		"all-dem-msr-runtimes"
+		"all-msr-runtimes"
 	)
-)
+) ## todo: make sure the picture actually fits on a page, width-wise
