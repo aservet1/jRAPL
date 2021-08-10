8bdcacdf43e33103786eae273170b3947f425303
alejandro <aservet1@binghamton.edu>
2021-08-10 16:18:48 -0400

    did the 2 plots, gotta get a good axis alignment though

--- tests/jmh/synchronous-energy-sampling/scripts/visualize-jmh-result.py
+++ tests/jmh/synchronous-energy-sampling/scripts/visualize-jmh-result.py
@@ -24,58 +24,66 @@ def plt_set_axis_limits(xrange, yrange, xaxis_precision, yaxis_precision):
 
 # if len(argv) != 2:
 try:
-    jmh_result = argv[1]
-    result_dir = argv[2]
+    result_dir = argv[1]
+    jmh_results = argv[2:]
 except:
-    print('usage:',argv[0],'jmh-result.json result_dir')
+    print('usage:',argv[0],'result_dir, jmh-result.json for System A, jmh-result.json for System B')
     exit(2)
 
-with open(jmh_result) as f:
-    _data = json.loads(f.read())
+system_names = ['System A', 'System B']
+fig, axs = plt.subplots(nrows=1, ncols=len(system_names))
+for i,jmh_result in enumerate(jmh_results):
+    print(jmh_result)
+    with open(jmh_result) as f:
+        _data = json.loads(f.read())
 
-data = {}
-for d in _data:
-    name = d['benchmark'].split('.')[-1]
-    data[name] = d
+    data = {}
+    for d in _data:
+        name = d['benchmark'].split('.')[-1]
+        data[name] = d
 
-unit    =  [];
-means   =  [];
-labels  =  [];
-errors  =  []; # not standard deviation. not sure what the error is
+    unit    =  [];
+    means   =  [];
+    labels  =  [];
+    errors  =  []; # not standard deviation. not sure what the error is
 
-for name in data:
-    if name.startswith('time'):
-        labels.append(name.replace('time','',1))
-    else:
-        labels.append(name)
-    means.append( data[name]['primaryMetric']['score'] )
-    errors.append( data[name]['primaryMetric']['scoreError'] )
-    unit.append( data[name]['primaryMetric']['scoreUnit'] )
+    for name in data:
+        if name.startswith('time'):
+            labels.append(name.replace('time','',1))
+        else:
+            labels.append(name)
+        means .append( data[name]['primaryMetric']['score'     ] )
+        errors.append( data[name]['primaryMetric']['scoreError'] )
+        unit  .append( data[name]['primaryMetric']['scoreUnit' ] )
 
-assert(sorted(unit) == sorted(unit)[-1::-1]) # idiom for 'all items equal'
-unit = unit[0]
+    assert(sorted(unit) == sorted(unit)[-1::-1]) # idiom for 'all items equal'
+    unit = unit[0]
 
-print(labels)
-print(means)
-print(errors)
-print('units:',unit)
+    print(labels)
+    print(means)
+    print(errors)
+    print('units:',unit)
 
-xrange = (None, None)
-yrange = (32, 36)
-xaxis_precision, yaxis_precision = (0, 0)
-plt_set_axis_limits(xrange, yrange, xaxis_precision, yaxis_precision)
+    # xrange = (None, None)
+    # yrange = (32, 36)
+    # xaxis_precision, yaxis_precision = (0, 0)
+    # plt_set_axis_limits(xrange, yrange, xaxis_precision, yaxis_precision)
 
-plt.bar(
-    range(len(labels)),
-    means,
-    yerr = errors,
-    tick_label = labels,
-    color = 'cadetblue',
-    edgecolor = 'black',
-    alpha = 1
-);
+    ax = axs.flat[i]
+    ax.bar (
+        range(len(labels)),
+        means,
+        yerr = errors,
+        tick_label = labels,
+        color = 'cadetblue',
+        edgecolor = 'black',
+        alpha = 1
+    );
 
-plt.ylabel('Average Runtime ({})'.format(unit))
-plt.xlabel('Sampling Version')
-# plt.title('average sample runtime'.title())
-plt.savefig(os.path.join(result_dir,'sync-samples-runtime'))
+    ax.set_title(system_names[i])
+    ax.set_ylabel('Average Runtime ({})'.format(unit))
+    ax.set_xlabel('Sampling Version')
+    # plt.title('average sample runtime'.title())
+
+plt.show()
+fig.savefig(os.path.join(result_dir,'sync-samples-runtime'))
