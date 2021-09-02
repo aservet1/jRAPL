f9811fe60d3cb5bc6433a510ecc4bee808d70cff
Alejandro Servetto <aservet1@binghamton.edu>
2021-09-02 04:03:10 -0400

    memory footprint represented as normalized, and as percent difference

--- tests/dacapo/async-monitors/scripts/visualization-multis-permonitor/memory-footprint.py
+++ tests/dacapo/async-monitors/scripts/visualization-multis-permonitor/memory-footprint.py
@@ -6,18 +6,39 @@ import matplotlib.pyplot as plt
 
 from myutil import plt_set_axis_limits, megaplot, validate_output_dir, get_data_files, output_dir
 
-keypath = ['overall', 'normalized']
+def normalized():
+    keypath = ['overall', 'normalized']
 
-fig = megaplot(get_data_files('memory-footprint'), keypath=keypath, color='limegreen', edgecolor='black', alpha=1)
+    fig = megaplot(get_data_files('memory-footprint'), keypath=keypath, color='limegreen', edgecolor='black', alpha=1)
 
-xrange = (None, None)
-yrange = (.9, 1.1)
-xaxis_precision, yaxis_precision = (0, 2)
-plt_set_axis_limits(xrange, yrange, xaxis_precision, yaxis_precision)
+    xrange = (None, None)
+    yrange = (.9, 1.1)
+    xaxis_precision, yaxis_precision = (0, 2)
+    plt_set_axis_limits(xrange, yrange, xaxis_precision, yaxis_precision)
 
-# fig.supxlabel('Monitor Type')
-# fig.supylabel('Normalized Memory Footprint')
+    # fig.supxlabel('Monitor Type')
+    # fig.supylabel('Normalized Memory Footprint')
 
-plt.show()
-validate_output_dir(output_dir)
-fig.savefig(os.path.join(output_dir,'memory-footprint'))
+    plt.show()
+    validate_output_dir(output_dir)
+    fig.savefig(os.path.join(output_dir,'normalized-memory-footprint'))
+
+def percent_difference():
+    keypath = ['overall', 'percentdiff']
+
+    fig = megaplot(get_data_files('memory-footprint'), keypath=keypath, color='limegreen', edgecolor='black', alpha=1)
+
+    xrange = (None, None)
+    yrange = (.9, 1.1)
+    xaxis_precision, yaxis_precision = (0, 2)
+    # plt_set_axis_limits(xrange, yrange, xaxis_precision, yaxis_precision)
+
+    # fig.supxlabel('Monitor Type')
+    # fig.supylabel('Normalized Memory Footprint')
+
+    plt.show()
+    validate_output_dir(output_dir)
+    fig.savefig(os.path.join(output_dir,'percentdiff-memory-footprint'))
+
+normalized()
+percent_difference()
