8021dcd50733ed2f9a39e65501c18e05609d0938
Alejandro Servetto <aservet1@binghamton.edu>
2021-09-02 03:40:40 -0400

    add entries to the script

--- tests/dacapo/async-monitors/2by3plots.sh
+++ tests/dacapo/async-monitors/2by3plots.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+./scripts/visualization-multis-permonitor/memory-footprint.py
+./scripts/visualization-multis-permonitor/observed-sample-count.py
+./scripts/visualization-multis-permonitor/observed-sample-interval.py
 ./scripts/visualization-multis-permonitor/normalized-sample-count.py
 ./scripts/visualization-multis-permonitor/normalized-sample-interval.py
-./scripts/visualization-multis-permonitor/memory-footprint.py
