e2e4c1091231928065139c404eb4ca064ee05731
Alejandro Servetto <aservet1@binghamton.edu>
2021-09-21 18:17:37 -0400

    small bugfixes

--- tests/dacapo/async-monitors/csv_reformatting/diff-csv.py
+++ tests/dacapo/async-monitors/csv_reformatting/diff-csv.py
@@ -25,17 +25,16 @@ class MultiprocessEngine(object):
             if key == 'timestamp': continue
             if 'dram' in key: wrap = DRAM_RAPL_WRAPAROUND
             else: wrap = NRML_RAPL_WRAPAROUND
-            diffdf[key] = diff_list(df[key],wrap)
+            diffdf[key] = diff_list(df[key],wrap=wrap)
 
-        diffdf.rename(columns={'timestamp':'time_elapsed'}, inplace = True)
         diffdf['start_timestamp'] = df['timestamp'].to_list()[:-1]
-        diffdf['elapsed_time'] = diff_list(df['timestamp'])
+        diffdf['time_elapsed'] = diff_list(df['timestamp'])
 
         diffdf.to_csv(os.path.join(dstdir,filename), index = False)
 
         print(' ~1)) '+filename)
  
-def diff_list(self, lst, wrap = 0):
+def diff_list(lst, wrap = 0):
     diffs = []
     for i in range(1,len(lst)):
         diff = lst[i] - lst[i-1]
